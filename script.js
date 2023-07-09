fb2083cfbd18b0acac3b5ef87f9749a941abf998
Vedansh Jain <vedansh.code@gmail.com>
2023-07-09 19:48:42 +0530

    Bug Fixed

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ setInterval( () => {
 let progress = document.getElementById('progressbar');
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function(){
-    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    let progressHeight = (window.scrollY / totalHeight) * 100;
     progress.style.height = progressHeight + "%";
 }
 
